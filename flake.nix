5ff8a03c2d5e2417bc2a47d6f68e408376d17745
Adam Sunderland <iterion@gmail.com>
2023-11-29 21:45:47 -0500

    First flake merge

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "flake for nix base";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      iterion-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.iterion = import ./home.nix;
+          }
+        ];
+
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,217 @@
+{ config, pkgs, ... }:
+
+let
+  mod = "Mod4";
+  ws-web = "1: Web";
+  ws-slack = "2: Slack";
+
+in 
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "iterion";
+  home.homeDirectory = "/home/iterion";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    google-chrome
+    alacritty
+    _1password-gui
+    htop
+    font-awesome
+    xclip
+    slack
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+  home.pointerCursor = {
+    package = pkgs.vanilla-dmz;
+    name = "Vanilla-DMZ";
+    size = 64;
+  };
+
+  xdg.enable = true;
+  xsession = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+      config = {
+        keybindings = {
+	  "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+	  "${mod}+d" = "exec ${pkgs.rofi}/bin/rofi -show run";
+	  "${mod}+Tab" = "exec ${pkgs.rofi}/bin/rofi -show window";
+	  "${mod}+Shift+apostrophe" = "kill";
+	  
+	  "${mod}+1" = "workspace ${ws-web}";
+	  "${mod}+2" = "workspace ${ws-slack}";
+	  "${mod}+3" = "workspace 3";
+	  "${mod}+4" = "workspace 4";
+	  "${mod}+5" = "workspace 5";
+	  "${mod}+6" = "workspace 6";
+	  "${mod}+7" = "workspace 7";
+	  "${mod}+8" = "workspace 8";
+	  "${mod}+9" = "workspace 9";
+	  "${mod}+0" = "workspace 10";
+	  "${mod}+Shift+1" = "move container to workspace ${ws-web}";
+	  "${mod}+Shift+2" = "move container to workspace ${ws-slack}";
+	  "${mod}+Shift+3" = "move container to workspace 3";
+	  "${mod}+Shift+4" = "move container to workspace 4";
+	  "${mod}+Shift+5" = "move container to workspace 5";
+	  "${mod}+Shift+6" = "move container to workspace 6";
+	  "${mod}+Shift+7" = "move container to workspace 7";
+	  "${mod}+Shift+8" = "move container to workspace 8";
+	  "${mod}+Shift+9" = "move container to workspace 9";
+	  "${mod}+Shift+0" = "move container to workspace 10";
+
+	  "${mod}+Shift+r" = "restart";
+	};
+        bars = [
+          {
+            position = "top";
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+          }
+        ];
+      };
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Adam Sunderland";
+    userEmail = "iterion@gmail.com";
+    aliases = {
+      co = "checkout";
+      amend = "commit -a --amend";
+    };
+    extraConfig = {
+      color = {
+        ui = "auto";
+      };
+      diff = {
+        tool = "vimdiff";
+        mnemonicprefix = true;
+      };
+      push = {
+        default = "simple";
+      };
+    };
+  };
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        icons = "awesome6";
+        blocks = [
+         {
+           block = "time";
+           interval = 60;
+           format = "$icon $timestamp.datetime(f:'%a %Y-%m-%d %R %Z')";
+         }
+       ];
+      };
+    };
+  };
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+    ];
+  };
+  programs.awscli = {
+    enable = true;
+    settings = {
+      "default" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "950941903565";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+      "admin-management" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "950941903565";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+      "admin-prod" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "598082278656";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+      "admin-dev" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "795424409989";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+      "admin-corp" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "704705872864";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+      "admin-executor" = {
+        "output" = "json";
+        "region" = "us-east-1";
+        "sso_account_id" = "211360623297";
+        "sso_region" = "us-east-1";
+        "sso_role_name" = "admin";
+        "sso_start_url" = "https://kittycad.awsapps.com/start#";
+      };
+    };
+  };
+
+
+  services.gnome-keyring.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+}
