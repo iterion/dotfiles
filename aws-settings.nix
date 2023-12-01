3e158c3e8fa6952ff63152389006ac9c96ebcada
Adam Sunderland <iterion@gmail.com>
2023-11-30 20:41:44 -0500

    Add aws-settings.nix

---
+++ aws-settings.nix
@@ -0,0 +1,50 @@
+{
+  "profile default" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "950941903565";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+  "profile admin-management" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "950941903565";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+  "profile admin-prod" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "598082278656";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+  "profile admin-dev" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "795424409989";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+  "profile admin-corp" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "704705872864";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+  "profile admin-executor" = {
+    "output" = "json";
+    "region" = "us-east-1";
+    "sso_account_id" = "211360623297";
+    "sso_region" = "us-east-1";
+    "sso_role_name" = "admin";
+    "sso_start_url" = "https://kittycad.awsapps.com/start#";
+  };
+}
