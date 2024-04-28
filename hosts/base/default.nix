e808cfd8da44cc3947ec445de8ba9d8baacd522a
Adam Sunderland <iterion@gmail.com>
2024-04-28 19:35:59 -0400

    Fix up overlays

--- hosts/base/default.nix
+++ hosts/base/default.nix
@@ -4,7 +4,6 @@
   ...
 }: {
   imports = [
-    ../../modules/phue
     ../../modules/lightsout
   ];
 
@@ -80,6 +79,24 @@
     allowUnfree = true;
     pulseaudio = true;
   };
+  nixpkgs.overlays = [ 
+    (final: prev: {
+      # nix-shell -p python.pkgs.my_stuff
+      python = prev.python.override {
+        # Careful, we're using a different final and prev here!
+        packageOverrides = final: prev: {
+          phue = prev.buildPythonPackage rec {
+            pname = "phue";
+            version = "1.1";
+            src = prev.fetchPypi {
+              inherit pname version;
+              hash = "0bp9bjqy1n6ij1zb86wz9lqa1dhla8qr1d7w2kxyn7jbj56sbmcw";
+            };
+          };
+        };
+      };
+    })
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
